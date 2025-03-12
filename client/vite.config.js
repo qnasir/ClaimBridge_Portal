ba0b4f36daf2d7e1c6ca975aac7f1adb95d3867f
qnasir57575 <qnasir57575@gmail.com>
2025-03-12 06:33:24 +0530

    Implemented tailwind in frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
+import path from "path";
+import { fileURLToPath } from "url"; 
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vitejs.dev/config/
+export default defineConfig(() => ({
+  server: {
+    host: "::",
+    port: 8000,
+  },
+  plugins: [tailwindcss(),react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
