aa23171cd51c09b6c455e03b713f4db9d677a757
qnasir57575 <qnasir57575@gmail.com>
2025-03-12 07:23:47 +0530

    Added shadCn for ui enchancement

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import {Button} from '@/components/ui/button'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <div className="flex items-center justify-center min-h-screen min-w-screen bg-gray-100">
         <h1 className="text-4xl font-bold text-red-600">Hello, Tailwind CSS!</h1>
+        <Button className="text-blue-600">Click Me</Button>
         </div>
       </>
       )
