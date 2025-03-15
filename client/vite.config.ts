3c0f36da38f365ce06ee7593099d5b1868d3b22d
qnasir57575 <qnasir57575@gmail.com>
2025-03-15 08:06:56 +0530

    Reduced chunk size under 500kb

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { imagetools } from "vite-imagetools";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -10,10 +11,26 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    imagetools(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 500, // Ensures chunks are below 500KB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react-vendor"; 
+            if (id.includes("recharts")) return "recharts-vendor"; 
+            if (id.includes("radix-ui")) return "radix-vendor"; 
+            if (id.includes("@tanstack/react-query")) return "react-query"; 
+          }
+        },
+      },
     },
   },
 }));
